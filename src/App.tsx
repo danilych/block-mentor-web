a81bec3e358952d9bc047973660f9768e5019e77
tamadekk <186jnd@gmail.com>
2025-03-21 13:55:40 +0100

    feat: create login component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,10 @@
-import { usePrivy } from "@privy-io/react-auth";
-import { Button } from "./components/ui/button";
 import ParticleEffect from "./components/Canvas";
+import Login from "./components/login";
 
 function App() {
-  const { ready, login } = usePrivy();
-  if (!ready) {
-    return <div>Loading...</div>;
-  }
-  const handleClick = () => {
-    login();
-  };
   return (
     <div className="min-h-svh flex items-center justify-center">
-      <div>
-        <h2 className="scroll-m-20 text-white pb-2 text-3xl font-semibold tracking-tight first:mt-0">
-          Welcome
-        </h2>
-        <Button onClick={handleClick}>Login</Button>
-      </div>
+      <Login />
       <ParticleEffect />
     </div>
   );
---
+++ src/components/login.tsx
@@ -0,0 +1,22 @@
+import { usePrivy } from "@privy-io/react-auth";
+import { Button } from "@/components/ui/button";
+
+function Login() {
+  const handleClick = () => {
+    login();
+  };
+  const { ready, login } = usePrivy();
+  if (!ready) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <div>
+      <h2 className="scroll-m-20 text-white pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+        Welcome
+      </h2>
+      <Button onClick={handleClick}>Login</Button>
+    </div>
+  );
+}
+
+export default Login;
