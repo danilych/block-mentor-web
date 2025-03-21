76e93d297e79ec004c053e762a3f554eaff373bb
tamadekk <186jnd@gmail.com>
2025-03-21 01:49:03 +0100

    feat: privy setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import './App.css'
+import "./App.css";
+import { usePrivy } from "@privy-io/react-auth";
 
 function App() {
-
+  const { ready, login } = usePrivy();
+  if (!ready) {
+    return <div>Loading...</div>;
+  }
+  const handleClick = () => {
+    login();
+  };
   return (
-    <>
-     <p>ETHWarsaw</p>
-    </>
-  )
+    <div>
+      <button
+        className="p-5 border-2 focus:bg-amber-400"
+        onClick={() => handleClick()}
+      >
+        Login
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
