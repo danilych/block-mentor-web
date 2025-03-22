6f651ade96cca13df23db1750082203a2e9aebc4
tamadekk <186jnd@gmail.com>
2025-03-23 00:52:20 +0100

    style: run prettier

--- src/service/client.ts
+++ src/service/client.ts
@@ -1,15 +1,15 @@
 import axios from 'axios'
-import { getAccessToken } from '@privy-io/react-auth';
-import { API_BASE_URL } from '@/config';
+import { getAccessToken } from '@privy-io/react-auth'
+import { API_BASE_URL } from '@/config'
 
-const $client = axios.create({baseURL: API_BASE_URL})
+const $client = axios.create({ baseURL: API_BASE_URL })
 
-$client.interceptors.request.use(async (config) => {
-  const token = await getAccessToken();
+$client.interceptors.request.use(async config => {
+  const token = await getAccessToken()
   if (token) {
-    config.headers["Authorization"] = `Bearer ${token}`;
+    config.headers['Authorization'] = `Bearer ${token}`
   }
-  return config;
-});
+  return config
+})
 
-export default $client;
+export default $client
