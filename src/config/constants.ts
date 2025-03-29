fbae0b6e96cf860f58b410fccf8b9a454190cdcf
tamadekk <186jnd@gmail.com>
2025-03-29 17:01:13 +0100

    refactor: update vesting URL from /bridge to /vesting

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,3 +1,4 @@
-export const APP_URL = 'https://block-mentor-platfrom-web-production.up.railway.app'
-
-export const getBridgeUrl = (tokenAddress: string) => `${APP_URL}/bridge/${tokenAddress}`
+export const APP_URL =
+  'https://block-mentor-platfrom-web-production.up.railway.app'
+export const getVestingeUrl = (tokenAddress: string) =>
+  `${APP_URL}/vesting/${tokenAddress}`
