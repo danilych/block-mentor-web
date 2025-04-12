bfed7a432a46b131268554ad88c16009d2bab12a
tamadekk <186jnd@gmail.com>
2025-04-12 12:47:05 +0200

    style: run format

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,23 +1,23 @@
 export interface ApiVesting {
-  id: string;
-  blockTimestamp: string;
-  token_name: string;
-  token_ticker: string;
-  amount: string;
-  total_periods: string;
-  period_duration: string;
-  start_timestamp: string;
-  token_address: string;
-  owner: string;
-  webpage?: string;
+  id: string
+  blockTimestamp: string
+  token_name: string
+  token_ticker: string
+  amount: string
+  total_periods: string
+  period_duration: string
+  start_timestamp: string
+  token_address: string
+  owner: string
+  webpage?: string
 }
 
 export interface ApiToken {
-  id: string;
-  blockTimestamp: string;
-  initialAmount: string;
-  name: string;
-  ticker: string;
-  owner: string;
-  token_address: string;
+  id: string
+  blockTimestamp: string
+  initialAmount: string
+  name: string
+  ticker: string
+  owner: string
+  token_address: string
 }
